98f923de8f737e01ba1f16c02567614680cb0362
Jean-Christophe Pennel <jcpennel@gmail.com>
2021-01-10 20:36:55 -0500

    Added Pre Compiled headers support (CMake)

---
+++ comet/src/core/cometpch.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include <comet/log.h>
+#include <comet/assert.h>
+
+// C++ Types
+#include <cstdint>
+
+// STL Types
+#include <string>
+
+// STL containers
+#include <vector>
+
+// STL others
+#include <memory>
+#include <sstream>
